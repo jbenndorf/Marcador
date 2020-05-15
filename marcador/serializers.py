3c3c0a4042b43f6d2ad7b881ddf46c6107fa4a76
Johanna <Jojo@Johannas-MacBook-Pro.local>
2020-05-15 11:10:43 +0100

    Bookmarks display tag string instead of ID

--- marcador/serializers.py
+++ marcador/serializers.py
@@ -1,14 +1,17 @@
-from marcador.models import Bookmark, Tag
 from rest_framework import serializers
+
+from .models import Bookmark, Tag
 
 
 class BookmarkSerializer(serializers.ModelSerializer):
+    tags = serializers.StringRelatedField(many=True)
+    
     class Meta:
-        model=Bookmark
-        fields=['url','title','description','is_public','date_created','date_updated','owner','tags']
+        model = Bookmark
+        fields = ['url', 'title', 'description', 'is_public', 'date_created', 'date_updated', 'owner', 'tags']
 
 
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
-        model=Tag
-        fields=['name']
+        model = Tag
+        fields = ['name']
