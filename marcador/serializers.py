68000cd696e98d10d4030d7bc3207e10df968953
Johanna <Jojo@Johannas-MacBook-Pro.local>
2020-05-15 20:56:48 +0100

    Smaller changes (no new features)

--- marcador/serializers.py
+++ marcador/serializers.py
@@ -5,7 +5,7 @@ from .models import Bookmark, Tag
 
 class BookmarkSerializer(serializers.ModelSerializer):
     tags = serializers.StringRelatedField(many=True)
-    
+
     class Meta:
         model = Bookmark
         fields = ['url', 'title', 'description', 'is_public', 'date_created', 'date_updated', 'owner', 'tags']
