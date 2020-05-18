758bd0fcdc1dca41f1aa33e2a0c1f8f6a6e0dd6a
Cezar Pendarovski <cezarpendarovski@web.de>
2020-05-18 16:13:15 +0100

    Pass tags when creating a Bookmark

--- marcador/serializers.py
+++ marcador/serializers.py
@@ -4,7 +4,11 @@ from .models import Bookmark, Tag
 
 
 class BookmarkSerializer(serializers.HyperlinkedModelSerializer):
-    tags = serializers.HyperlinkedRelatedField(many=True, view_name='tag-detail', read_only=True)
+    tags = serializers.HyperlinkedRelatedField(
+        view_name='tag-detail',
+        queryset=Tag.objects.all(),
+        many=True,
+    )
     owner = serializers.CharField(source='bookmark.owner', read_only=True)
     date_created = serializers.DateTimeField(read_only=True)
     date_updated = serializers.DateTimeField(read_only=True)
