531c473ea167c3b387cafaa75349f6f432fe02ee
Cezar Pendarovski <cezarpendarovski@web.de>
2020-05-21 16:01:36 +0100

    Add tests for BookmarkViewSet

--- marcador_api/tests.py
+++ marcador_api/tests.py
@@ -8,8 +8,8 @@ from marcador.models import Bookmark, Tag
 
 
 class TagViewSetTestCase(APITestCase):
-    list = 'marcador_api:tag-list'
-    detail = 'marcador_api:tag-detail'
+    list_view = 'marcador_api:tag-list'
+    detail_view = 'marcador_api:tag-detail'
 
     def setUp(self):
         self.tag = Tag.objects.create(name='test')
@@ -28,7 +28,7 @@ class TagViewSetTestCase(APITestCase):
         Not authenticated users should be able to perform the list
         operation on the endpoint tags.
         """
-        response = self.client.get(reverse(self.list))
+        response = self.client.get(reverse(self.list_view))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
 
@@ -45,7 +45,7 @@ class TagViewSetTestCase(APITestCase):
         operation on the endpoint tags
         """
         response = self.client.post(
-            reverse(self.list),
+            reverse(self.list_view),
             {'name': 'create'},
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
@@ -56,7 +56,7 @@ class TagViewSetTestCase(APITestCase):
         operation on the endpoint tags
         """
         response = self.client.put(
-            reverse(self.detail, kwargs={'pk': 1}),
+            reverse(self.detail_view, kwargs={'pk': 1}),
             {'name': 'update'},
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
@@ -74,7 +74,7 @@ class TagViewSetTestCase(APITestCase):
         operation on the endpoint tags
         """
         self.client.force_login(user=self.user)
-        response = self.client.get(reverse(self.list))
+        response = self.client.get(reverse(self.list_view))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
 
@@ -92,7 +92,7 @@ class TagViewSetTestCase(APITestCase):
         """
         self.client.force_login(user=self.user)
         response = self.client.post(
-            reverse(self.list),
+            reverse(self.list_view),
             {'name': 'create'}
         )
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
@@ -117,7 +117,7 @@ class TagViewSetTestCase(APITestCase):
         on the endpoint tags
         """
         self.client.force_login(user=self.superuser)
-        response = self.client.get(reverse(self.list))
+        response = self.client.get(reverse(self.list_view))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
 
@@ -127,7 +127,7 @@ class TagViewSetTestCase(APITestCase):
         on the endpoint tags
         """
         self.client.force_login(user=self.superuser)
-        response = self.client.get(reverse(self.detail, kwargs={'pk': 1}))
+        response = self.client.get(reverse(self.detail_view, kwargs={'pk': 1}))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['name'], 'test')
 
@@ -138,7 +138,7 @@ class TagViewSetTestCase(APITestCase):
         """
         self.client.force_login(user=self.superuser)
         response = self.client.post(
-            reverse(self.list),
+            reverse(self.list_view),
             {'name': 'create'}
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -150,7 +150,7 @@ class TagViewSetTestCase(APITestCase):
         """
         self.client.force_login(user=self.superuser)
         response = self.client.put(
-            reverse(self.detail, kwargs={'pk': 1}),
+            reverse(self.detail_view, kwargs={'pk': 1}),
             {'name': 'update'}
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -163,6 +163,118 @@ class TagViewSetTestCase(APITestCase):
         """
         self.client.force_login(user=self.superuser)
         response = self.client.delete(
-            reverse(self.detail, kwargs={'pk': 1})
+            reverse(self.detail_view, kwargs={'pk': 1})
         )
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+
+class BookmarkViewSetTestCase(APITestCase):
+    list_view = 'marcador_api:bookmark-list'
+    detail_view = 'marcador_api:bookmark-detail'
+
+    def setUp(self):
+        self.user_a = User.objects.create(username='testA', password='pass123')
+        self.user_b = User.objects.create(username='testB', password='pass456')
+        self.superuser = User.objects.create(
+            username='admin',
+            password='adminpass',
+            is_superuser=True,
+        )
+        self.test_tag_a = Tag.objects.create(name='testA')
+        self.test_tag_b = Tag.objects.create(name='testB')
+        self.public_a = Bookmark.objects.create(
+            bookmark_url='http://example.com/',
+            title='example',
+            description='This is just an example',
+            owner=self.user_a,
+        )
+        self.public_a.tags.add(self.test_tag_a)
+        self.private_a = Bookmark.objects.create(
+            bookmark_url='http://example.co.uk/',
+            title='example uk',
+            is_public=False,
+            owner=self.user_a,
+        )
+        self.private_a.tags.add(self.test_tag_a)
+        self.public_b = Bookmark.objects.create(
+            bookmark_url='http://example.fr/',
+            title='example fr',
+            description='Une example',
+            owner=self.user_b,
+        )
+        self.public_b.tags.add(self.test_tag_b)
+        self.private_b = Bookmark.objects.create(
+            bookmark_url='http://example.de/',
+            title='Beispiel',
+            is_public=False,
+            owner=self.user_b,
+        )
+        self.private_b.tags.add(self.test_tag_b)
+
+    def test_user_can_read_public_bookmarks(self):
+        """
+        All users should be able to read the public bookmarks.
+        """
+        response = self.client.get(reverse(self.list_view))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_user_can_read_public_bookmark(self):
+        """
+        All users should be able to read a public bookmark.
+        """
+        response = self.client.get(
+            reverse(self.detail_view, kwargs={'pk': 1})
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], 'example')
+
+    def test_user_cannot_read_private_bookmark(self):
+        """
+        Users should not be able to read a private
+        bookmark that doesn't belong to them.
+        """
+        response = self.client.get(
+            reverse(self.detail_view, kwargs={'pk': 2})
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_user_can_edit_own_bookmark(self):
+        """
+        An authenticated user should be able to edit his/her own
+        bookmark.
+        """
+        self.client.force_login(user=self.user_a)
+        response = self.client.put(
+            reverse(self.detail_view, kwargs={'pk': 1}),
+            {'bookmark_url': 'https://www.example.com/', 'title': 'Example'}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['bookmark_url'], 'https://www.example.com/')
+        self.assertEqual(response.data['title'], 'Example')
+
+    def test_user_cannot_edit_foreign_bookmark(self):
+        """
+        An authenticated user should be able to edit his/her own
+        bookmark.
+        """
+        self.client.force_login(user=self.user_b)
+        response = self.client.put(
+            reverse(self.detail_view, kwargs={'pk': 1}),
+            {'bookmark_url': 'https://www.example.com/', 'title': 'Example'}
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_superuser_can_edit_bookmark(self):
+        """
+        A superuser should be able to edit a public bookmark from any
+        user.
+        """
+        self.client.force_login(user=self.superuser)
+        response = self.client.put(
+            reverse(self.detail_view, kwargs={'pk': 1}),
+            {'bookmark_url': 'https://www.example.com/', 'title': 'Example'}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['bookmark_url'], 'https://www.example.com/')
+        self.assertEqual(response.data['title'], 'Example')
