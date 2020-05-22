abd66f17f727fcb7d034cdda4f8338446b099e1d
Cezar Pendarovski <cezarpendarovski@web.de>
2020-05-22 12:03:06 +0100

    Create tests module and split tests.py

---
+++ marcador/tests/__init__.py
@@ -0,0 +1,8 @@
+from .models import TagTestCase, BookmarkTestCase
+from .views import (
+    BookmarkListTestCase,
+    UserBookmarkListTestCase,
+    BookmarkCreateTestCase,
+    BookmarkUpdateTestCase,
+    BookmarkDeleteTestCase
+)
